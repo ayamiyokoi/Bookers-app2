f1c4fcd6e30a0ce9196fe1e202adf59faa231030
AyamiYokoi <didufd63hff@gmail.com>
2021-05-06 15:50:11 +0000

    [Add]投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,30 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
   def create
+    book = Book.new(book_params)
+    # ２. データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    # ３. トップ画面へリダイレクト
+    redirect_to book_path(book.id)
   end
 
   def index
+    @books=Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def destroy
+  end
+  
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get "/" => 'homes#top'
+  get "homes/top" => 'homes#top'
+  get "homes/about" =>'homes#about'
+  post 'books' => 'books#create'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit, :update]
 end
