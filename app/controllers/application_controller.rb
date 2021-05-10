374481bfd842b44e3f8cc6594801e80041cfd0fc
AyamiYokoi <didufd63hff@gmail.com>
2021-05-10 02:38:53 +0000

    念のため保存

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,22 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    books_path
+    @user1 = current_user
+    user_path(@user1)
+  end
+
+  def after_sign_out_path_for(resource)
+    homes_about_path
   end
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:introduction])
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :introduction])
   end
 
 
