63ea81a98a1800386b5fe7ca4087b869b2d91a9f
AyamiYokoi <didufd63hff@gmail.com>
2021-05-11 13:34:23 +0000

    [finish]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'fe31f7ef9b906714d94c0151ae2863d52dbf57f0e2723f8e79f95fab30a68a9972fd5e2c87f92a154d75ebaea8835f223db793e91191bcbe6a998a5729d63ae6'
